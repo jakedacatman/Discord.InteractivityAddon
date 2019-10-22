9c55aca1b74fb62581fabfee9679e45d30384ef1
Elias Wolf <eliaswolf2001@t-online.de>
2019-10-22 13:28:44 +0200

    Fixed potential nullref if user or message is not specified

--- InteractivityAddon/Extensions/SocketReactionExtensions.cs
+++ InteractivityAddon/Extensions/SocketReactionExtensions.cs
@@ -8,8 +8,12 @@ namespace Interactivity.Extensions
         public static async Task DeleteAsync(this SocketReaction reaction, BaseSocketClient client)
         {
             var channel = reaction.Channel;
-            var message = reaction.Message.Value ?? await channel.GetMessageAsync(reaction.MessageId).ConfigureAwait(false) as SocketUserMessage;
-            var user = reaction.User.Value ?? client.GetUser(reaction.UserId);
+            var message = reaction.Message.IsSpecified
+                ? reaction.Message.Value
+                :await channel.GetMessageAsync(reaction.MessageId).ConfigureAwait(false) as SocketUserMessage;
+            var user = reaction.User.IsSpecified 
+                ? reaction.User.Value
+                : client.GetUser(reaction.UserId);
 
             await message.RemoveReactionAsync(reaction.Emote, user).ConfigureAwait(false);
         }
