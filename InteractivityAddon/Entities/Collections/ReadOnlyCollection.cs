11798d4a749ff800f1b67e0fd5852f904e263cb9
Playwo <eliaswolf2001@t-online.de>
2021-01-26 18:37:08 +0100

    Remove dependency on Qommon

---
+++ InteractivityAddon/Entities/Collections/ReadOnlyCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Interactivity
+{
+    internal sealed class ReadOnlyCollection<T> : IReadOnlyCollection<T>
+    {
+        public int Count => _collection.Count;
+
+        private readonly ICollection<T> _collection;
+
+        public ReadOnlyCollection(ICollection<T> collection)
+        {
+            if (collection is null)
+            {
+                throw new ArgumentNullException(nameof(collection));
+            }
+
+            _collection = collection;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+            => _collection.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+    }
+}
--- InteractivityAddon/Extensions/CollectionExtensions.cs
+++ InteractivityAddon/Extensions/CollectionExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using Qommon.Collections;
 
 namespace Interactivity.Extensions
 {
@@ -9,9 +7,6 @@ namespace Interactivity.Extensions
     {
         public static ReadOnlyCollection<T> AsReadOnlyCollection<T>(this IEnumerable<T> collection)
             => new ReadOnlyCollection<T>(collection.ToArray());
-
-        public static ReadOnlyList<T> AsReadOnlyList<T>(this IEnumerable<T> collection)
-            => new ReadOnlyList<T>(collection.ToArray());
 
         public static ReadOnlyDictionary<TKey, TValue> AsReadOnlyDictionary<TKey, TValue>(this Dictionary<TKey, TValue> dictionary)
             => new ReadOnlyDictionary<TKey, TValue>(dictionary);
